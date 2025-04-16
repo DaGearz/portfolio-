c6f56a18d44f652953a6f4a36e66f579c14013f9
DaGearz <twill003@gmail.com>
2025-04-16 10:38:58 -0700

    completed scaling for header. time to work on landing overlay

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -10,7 +10,26 @@ export default function MainNav() {
                 <li className={styles.li2}><a href="#">About</a></li>
                 <li className={styles.li3}><a href="#">Contact</a></li>
                 <li className={styles.li4}><a href="#">Portfolio</a></li>
-                <li className={styles.li5}><a href="#">Features</a></li>
+                <li className={styles.li5}><a href="#">Resume</a></li>
+                <li className={styles.li6}><a href="#">Features</a></li>
+                <li className={styles.li7}><select>
+                        <option className={styles.o1} value= "default" default>
+                            Additional
+                        </option>
+                        <option className={styles.o2} value= "about">
+                         About
+                        </option>
+                        <option className={styles.o3} value= "features">
+                            Features
+                        </option>
+                        <option className={styles.o4} value= "resume">
+                            Resume
+                        </option>
+                        
+                        
+
+                    </select>
+                </li>
             </ul> 
         </nav>
     )
