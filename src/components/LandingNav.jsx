054e06842d464988a583fdb662b60ba1418d34a8
DaGearz <twill003@gmail.com>
2025-05-05 13:04:25 -0700

    One more section to complete for Home

--- src/components/LandingNav.jsx
+++ src/components/LandingNav.jsx
@@ -1,18 +1,23 @@
-
+import { Link } from "react-router-dom";
 import styles from "../modules.css/LandingNav.module.css";
 
-export default function LandingNav() {
-
-     return(
-            <nav className={styles.nav}>
-                <ul className={styles["nav-list"]}>
-                    <li className={styles.li1}><a href="./home">Home</a></li>
-                    <li className={styles.li2}><a href="./about">About</a></li>
-                    <li className={styles.li3}><a href="./contact-info">Contact</a></li>
-                    <li className={styles.li4}><a href="./portfolio">Portfolio</a></li>
-                    
-                </ul> 
-            </nav>
-        )
-    }
-
+export default function LandingNav({ handleClose }) {
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles["nav-list"]}>
+        <li className={styles.li1}>
+          <Link to="/" onClick={handleClose}>Home</Link>
+        </li>
+        <li className={styles.li2}>
+          <Link to="/about" >About</Link>
+        </li>
+        <li className={styles.li3}>
+          <Link to="/contact-info" >Contact</Link>
+        </li>
+        <li className={styles.li4}>
+          <Link to="/portfolio" >Portfolio</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,50 +2,45 @@ import { useState } from "react";
 import MainLayout from "../templates/MainLayout";
 import LandingOverlay from "../components/LandingOverlay";
 import styles from "../modules.css/Home.module.css";
-import {greetings, buttonQuotes} from "../data/greetingsData"
+import { greetings, buttonQuotes } from "../data/greetingsData";
 
 export default function Home() {
-  const [showOverlay, setShowOverlay] = useState(true); // default: visible
+  const [showOverlay, setShowOverlay] = useState(false);
 
-  const [greeting , setGreeting] = useState(`Hi, my name is Todd — but online, I go by my OG gamertag: DaGearz. I’m kicking off my career as a software developer, and this site is just the beginning. Check out what I can do, and if you like what you see, don’t be shy — reach out! I’m ready to work, build, and grow with the right team.
-`)
-  const [qoutes, setQoutes] = useState("This intro kinda sucks, not gonna lie. Smash that button and roll the dice on a better one."
-)
-  
+  const [greeting, setGreeting] = useState(`Hi, my name is Todd — but online, I go by my OG gamertag: DaGearz. I’m kicking off my career as a software developer, and this site is just the beginning. Check out what I can do, and if you like what you see, don’t be shy — reach out! I’m ready to work, build, and grow with the right team.`);
+  const [quotes, setQuotes] = useState("This intro kinda sucks, not gonna lie. Smash that button and roll the dice on a better one.");
+
   const randomGreeting = () => {
-    const index = Math.floor(Math.random() * greetings.length);
-    setGreeting(greetings[index]);
-    setQoutes(buttonQuotes[index])
-  }
-
-  const handleOpen = () => {
-    setShowOverlay(true)
-  }
+    const indexG = Math.floor(Math.random() * greetings.length);
+    const indexQ = Math.floor(Math.random() * buttonQuotes.length);
+    setGreeting(greetings[indexG]);
+    setQuotes(buttonQuotes[indexQ]);
+  };
 
   return (
     <MainLayout>
       <main className={styles.Home}>
-
         <div className={`${styles.homeContainer}`}>
           <div>{greeting}</div>
-          <p>{qoutes}</p>
+          <p>{quotes}</p>
           <button onClick={randomGreeting} className={styles.ranButton} type="button">
             Randomize
           </button>
         </div>
+
         <div className={`${styles.homeContainer} ${styles.featured}`}>
-            <p>Click to check out my Featured Projects</p>
-            <div><span onClick= {handleOpen}>Featured Projects</span></div>
+          <p>Click to check out my Featured Projects</p>
+          <div>
+            <span onClick={() => setShowOverlay(true)}>Featured Projects</span>
+          </div>
         </div>
 
-        
         {showOverlay && (
           <div className={styles["landing-overlay"]}>
             <LandingOverlay handleClose={() => setShowOverlay(false)} />
           </div>
         )}
       </main>
-      
     </MainLayout>
   );
 }
