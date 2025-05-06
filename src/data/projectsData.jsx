96b74774f7ee1b97574ce1f68a08f4c0275b0065
DaGearz <twill003@gmail.com>
2025-05-05 19:16:58 -0700

    Save point for the day. Tommorrow will work on porfolio

---
+++ src/data/projectsData.jsx
@@ -0,0 +1,38 @@
+
+const projectsData = [
+    {
+        title:null,
+        discription:null,
+        img: {
+            img:null,
+            exist: true
+        },
+        img:{
+            variable:null;
+            exist: false;
+        }
+        tech: [null],
+        git1:{
+            git:null,
+            exist: true
+        },
+        git2:{
+            git:null,
+            exist: true
+        },
+        website:{
+            address:null;
+            exist: true
+        }
+        use: true
+
+
+    }
+
+]
+
+//use <summary> <detail> latter on
+//<iframe src="https://example.com" width="600" height="400"></iframe>
+//<img 
+
+export default projectsData
