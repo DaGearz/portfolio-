7dbf6f44586ccdf47bad3fda628829acaf755d8c
DaGearz <twill003@gmail.com>
2025-05-07 15:09:52 -0700

    more production setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-/' : '/',
+  base: './', // ✅ works for local dev and custom domain builds
   plugins: [react()],
 })
